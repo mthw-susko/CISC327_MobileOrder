189c008c98ff55d3e8722023d511a29ded9be624
Matthew Susko <mattew@metronomics.com>
2023-11-23 13:57:15 -0500

    White box order test fix

--- src/test/whiteBoxOrderTest.py
+++ src/test/whiteBoxOrderTest.py
@@ -7,16 +7,15 @@ from controller.orderManager import OrderManager
 # Fixture to create a OrderManager instance
 @pytest.fixture
 def order_manager():
-    return OrderManager
+    return OrderManager()
 
 # True Block validation
 def test_valid_cart_and_order_not_submitted(order_manager):
-    cart = OrderManager.cart
-    is_order_submitted = False
-    assert order_manager.submitOrder(cart, is_order_submitted) == True
+    order_manager.cart = ["item1", "item2"]
+    order_manager.is_order_submitted = False
+    assert order_manager.submitOrder() == True
 
 # False block validation
 def test_invalid_cart_and_order_not_submitted(order_manager):
-    cart = None
-    is_order_submitted = True
-    assert order_manager.submitOrder(cart, is_order_submitted) == False
+    order_manager.is_order_submitted = True
+    assert order_manager.submitOrder() == False
